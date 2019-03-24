a6c42c5fa69707a2471b67da7b6c429a964bcb99
Aii2BNice <3277485706@qq.com>
2019-03-24 08:00:34 +0800

    ...

--- AchievementConversion_springboot/src/main/java/com/dyq/user/domain/UserMapper.java
+++ AchievementConversion_springboot/src/main/java/com/dyq/user/domain/UserMapper.java
@@ -8,14 +8,31 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
 	
 	Login login(Login login);
+	
 	List<User> queryAll(User user);
 	List<Login> queryAllLogin(Login login);
+	
 	User queryUserById(Integer userId);
 	Login queryLoginByName(String loginName);
+	Admin queryAdminById(Integer adminId);
+	Customer queryCustomerById(Integer customerId);
+	
 	int insertLogin(Login login);
 	int insertUser(User user);
+	int insertAdmin(Admin admin);
+	int insCustomer(Customer customer);
+	
+	
 	int updateLogin(Login login);
 	int updateUser(User user);
+	int updatAdmin(Admin admin);
+	int updateCustomer(Customer customer);
+	
 	int deleteLogin(Integer loginId);
+	int deleteUser(User user);
+	int deleteAdmin(Admin admin);
+	int deleteCustomrt(Customer customer);
+	
+	
 	
 }
